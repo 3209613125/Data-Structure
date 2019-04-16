501328a6c467f5df598255a65253055800881b36
核桃圆 <47206212+3209613125@users.noreply.github.com>
2019-04-16 18:29:42 +0800

    Update 猴子吃桃.java

--- 递归/猴子吃桃.java
+++ 递归/猴子吃桃.java
@@ -30,3 +30,18 @@ class Test
         }
     }
 }
+
+
+//方法2 循环
+class Test {
+    public static void main(String[] args) {
+        int sum = 0, remain = 1;
+//每天吃剩的桃子加一个正好是前一天桃子的一半，每天桃子的总数就是前一天剩下桃子的数量
+        for (int day = 9; day >= 1; day--) {
+            sum = (remain + 1) * 2;
+            remain = sum;
+            System.out.println("第" + day + "天还剩" + remain + "个桃子");
+        }
+        System.out.println(sum);
+    }
+}
