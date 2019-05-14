5c2bd9682316c2245f1a8c1fd05befde8610a0d4
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-05-15 00:01:59 +0800

    Update 顺序表.java

--- 链表和顺序表/顺序表.java
+++ 链表和顺序表/顺序表.java
@@ -1,9 +1,38 @@
-public interface ISequence {
-    //在pos位置插入data
-    boolean add(int pos, Object data);
-    //查找关键字key，找到返回key的下标，没有找到返回-1
+package www.bit;
+
+import java.util.Arrays;
+
+class Test1 {
+    public static void main(String[] args) throws InterruptedException {
+        MySequenceImpl mySequence = new MySequenceImpl();
+
+       for (int i = 0; i < 20; i++) {
+            mySequence.add(i,i);
+        }
+        mySequence.add(19,"gao");
+//        mySequence.add(0,new Person());
+//        mySequence.add(1,new Person());
+//        mySequence.add(2,new Person());
+        mySequence.display();
+
+        mySequence.clear();
+        Thread.sleep(1000);
+
+
+        //mySequence.display();
+        //Object res = mySequence.remove(8);
+        //System.out.println(res);
+        //JAVA 里面如何测试内存泄漏
+        //mySequence.display();
+
+    }
+}
+interface ISequence {
+    //在pos位置插入val
+    boolean add(int pos,Object data);
+    //查找关键字key 找到返回key的下标，没有返回-1;
     int search(Object key);
-    //查找是否包含关键字key
+    //查找是否包含关键字key是否在顺序表当中(这个和search有点冲突)
     boolean contains(Object key);
     //得到pos位置的值
     Object getPos(int pos);
@@ -16,15 +45,12 @@ public interface ISequence {
     //清空顺序表以防内存泄漏
     void clear();
 }
-import java.util.Arrays;
 
-public class MySequenceImpl implements ISequence {
 
-    //存放顺序表的数组
+class MySequenceImpl implements ISequence {
+
     private Object[] elem;
-    //有效数据个数
-    private int usedSize;
-    //能存放数据的最大长度
+    private int usedSize;//有效数据个数
     private static final int DEFAULT_SIZE = 10;
 
     public MySequenceImpl() {
@@ -33,27 +59,27 @@ public class MySequenceImpl implements ISequence {
     }
 
     private boolean isFull() {
+        /*if(this.usedSize == this.elem.length) {
+            return true;
+        }
+        return false;*/
         return this.usedSize == this.elem.length;
-    }
-
-    private boolean isEmpty() {
-        return this.usedSize == 0;
     }
 
     @Override
     public boolean add(int pos, Object data) {
-        //判断pos的合法性
+        //1、pos位置的合法性进行判断
         if(pos < 0 || pos > this.usedSize) {
             return false;
         }
-        //判断是否为满
-        if(isFull()) {
+        //2、判断是否为满。
+        if(isFull()){
             //2倍扩容
-            this.elem = Arrays.copyOf(this.elem, 2*this.elem.length);
-
+            //System.arraycopy();
+            this.elem = Arrays.copyOf(this.elem,
+                    2*this.elem.length);
         }
-        //插入
-        for(int i = usedSize-1; i >= pos; i--) {
+        for (int i = this.usedSize-1; i >= pos; i--) {
             this.elem[i+1] = this.elem[i];
         }
         this.elem[pos] = data;
@@ -61,14 +87,16 @@ public class MySequenceImpl implements ISequence {
         return true;
     }
 
-
+    private boolean isEmpty() {
+        return this.usedSize == 0;
+    }
     @Override
     public int search(Object key) {
-        if(isEmpty()) {
+        if(isEmpty()){
             return -1;
         }
-        for(int i = 0; i < this.usedSize; i++) {
-            if(this.elem[i].equals(key)) {
+        for (int i = 0; i < this.usedSize; i++) {
+            if(this.elem[i].equals(key)){
                 return i;
             }
         }
@@ -77,11 +105,11 @@ public class MySequenceImpl implements ISequence {
 
     @Override
     public boolean contains(Object key) {
-        if(isEmpty()) {
+        if(isEmpty()){
             return false;
         }
-        for(int i = 0; i < this.usedSize; i++) {
-            if(this.elem[i].equals(key)) {
+        for (int i = 0; i < this.usedSize; i++) {
+            if(this.elem[i].equals(key)){
                 return true;
             }
         }
@@ -89,34 +117,27 @@ public class MySequenceImpl implements ISequence {
     }
 
     @Override
-    //得到pos位置的值
     public Object getPos(int pos) {
-        //判断是否为空
-        if(isEmpty()) {
-            return false;
-        }
-        //判断pos的合法性
-        if(pos < 0 || pos >= usedSize) {
+        if(pos < 0 || pos >= this.usedSize){
             return null;
         }
         return this.elem[pos];
     }
 
     @Override
-    //删除第一次出现的关键字key，不是则删除key位置上的元素
     public Object remove(Object key) {
-        //获取值为key的索引
         int index = search(key);
         if(index == -1) {
             return null;
         }
-
-        for(int i = index+1; i <= this.usedSize-1; i++) {
-            this.elem[i-1] = this.elem[i];
+        Object data = this.elem[index];
+        int i;
+        for ( i = index; i < this.usedSize-1; i++) {
+            this.elem[i] = this.elem[i+1];
         }
-        this.elem[usedSize-1] = null;
         this.usedSize--;
-        return true;
+        this.elem[i+1] = null;
+        return data;
     }
 
     @Override
@@ -126,9 +147,10 @@ public class MySequenceImpl implements ISequence {
 
     @Override
     public void display() {
-        for(int i = 0; i < this.usedSize; i++) {
+        for (int i = 0; i < this.usedSize; i++) {
             System.out.print(this.elem[i]+" ");
         }
+        System.out.println();
     }
 
     @Override
@@ -136,26 +158,5 @@ public class MySequenceImpl implements ISequence {
         for (int i = 0; i < this.usedSize; i++) {
             this.elem[i] = null;
         }
-    }
-}
-public class TestSequence {
-
-    public static void main(String[] args) {
-
-        ISequence sequence = new MySequenceImpl();
-        for(int i = 0; i < 20; i++) {
-            sequence.add(i, i);
-        }
-        sequence.display();
-        System.out.println();
-        System.out.println(sequence.search(19));
-        System.out.println(sequence.contains(18));
-        System.out.println(sequence.getPos(3));
-        System.out.println(sequence.size());
-        sequence.remove(5);
-        sequence.display();
-        sequence.clear();
-        System.out.println();
-        sequence.display();
     }
 }
