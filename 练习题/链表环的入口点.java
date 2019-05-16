1ae68ed5d10ccba7d323218fb69d3bcade78ab3d
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-05-17 01:56:56 +0800

    Create 链表环的入口点.java

---
+++ 练习题/链表环的入口点.java
@@ -0,0 +1,22 @@
+    //链表的环的入口点
+    public Node detectCycle() {
+        Node fast = this.head;
+        Node slow = this.head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) {
+                break;
+            }
+        }
+        //没有环  返回null
+        if(fast == null || fast.next == null){
+            return null;
+        }
+        slow = this.head;
+        while(fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
