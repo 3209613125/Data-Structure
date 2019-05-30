ba65a8ef48f6856b0543c95f5a6c4025b35e619f
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-05-30 13:49:06 +0800

    Update day_2 (2) .java

--- 每日一题/day_2 (2) .java
+++ 每日一题/day_2 (2) .java
@@ -3,3 +3,43 @@
 // 每个物品的体积分别是a1，a2……an。John可以从这些物品中选择一些，
 // 如果选出的物体的总体积是40，那么利用这个神奇的口袋，John就可以得到这些物品。
 // 现在的问题是，John有多少种不同的选择物品的方式。
+//采用递归思想： ①物品n个，物品体积逐一放入weight[]中 ②递归函数count(int s,int n) : 其
+//中s为剩余物品重量，n为剩余可选的物品个数
+//则分以下两步递归求解：
+//a.从后往前装，装上weight[n]后，若剩余物品仍然有解
+//则count(s-weight[n],n-1);
+//b.若装了weight[n]后，无解，则删除该包,尝试第n-1个
+//count(s,n-1);
+
+
+
+import java.util.*;
+ class Main {
+    static int[] weight;
+    static int N;
+    static int count=0;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            N = input.nextInt();
+            weight = new int[N+1];
+            for (int i = 1; i <= N; i++) {
+                weight[i] = input.nextInt();
+            }
+            count(40,N);
+            System.out.println(count);
+        }
+    }
+    public static void count(int s,int n) {
+//如果正好装满
+        if(s==0) {
+            ++count;
+            return ;
+        }
+//是s<0或n<1则不能完成
+        if(s<0||(s>0&&n<1))
+            return ;
+        count(s-weight[n],n-1);
+        count(s,n-1);
+    }
+}
