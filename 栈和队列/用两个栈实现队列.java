63505189c74d800ef88481c398f4114716589d6f
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-06-02 16:10:16 +0800

    Update 用两个栈实现队列.java

--- 栈和队列/用两个栈实现队列.java
+++ 栈和队列/用两个栈实现队列.java
@@ -1,11 +1,6 @@
 import java.util.Stack;
 
-/**
- * Description: 两个栈实现一个队列
- * Author: Administrator
- * Date: 2019/6/1
- * Time: 15:32
- */
+
 public class TestMyQueue {
 
     Stack<Integer> stack1 = new Stack<>();
