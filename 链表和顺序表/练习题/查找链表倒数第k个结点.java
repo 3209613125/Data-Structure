0bda73829ed115616f2efe036d83b655e170f7cd
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-06-03 11:26:58 +0800

    Create 查找链表倒数第k个结点.java

---
+++ 链表和顺序表/练习题/查找链表倒数第k个结点.java
@@ -0,0 +1,24 @@
+//输入一个链表，输出该链表中倒数第k个结点
+    public Node findKthToTail(int k){
+        if(k<=0||this.head==null){
+           return null;
+        }
+        Node fast=this.head;
+        Node slow=this.head;
+        int count=0;
+        while (count<k-1){
+            if(fast.next!=null){
+                fast=fast.next;
+                k--;
+            }
+            else{
+                System.out.println("没有这个节点");
+                return null;
+            }
+        }
+        while(fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        return slow;
+    }
