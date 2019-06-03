3fe42c89eca15a9a092966ca6159787b5ce63043
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-06-03 11:30:46 +0800

    Update 链表环的入口点.java

--- 链表和顺序表/练习题/链表环的入口点.java
+++ 链表和顺序表/练习题/链表环的入口点.java
@@ -1,22 +1,25 @@
- //链表的环的初始入口点
-    public Node detectCycle() {
+//链表的环的入口点
+public Node detectCycle(){
         Node fast = this.head;
         Node slow = this.head;
-        while(fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-            if(fast == slow) {
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
                 break;
             }
         }
-        //没有环  返回null
-        if(fast == null || fast.next == null){
+        //无环，返回null
+        if ((fast==null||fast.next==null)){
             return null;
         }
         slow = this.head;
-        while(fast != slow) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-        return fast;
+        while(fast!=slow){
+            fast=fast.next;
+            slow=slow.next;
     }
+    return fast;
+}
+
+
+}
