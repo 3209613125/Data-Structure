680190b0b228ccd45d39cf24e5940a8910b0ea28
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-06-03 11:28:32 +0800

    Update 判断有无环.java

--- 链表和顺序表/练习题/判断有无环.java
+++ 链表和顺序表/练习题/判断有无环.java
@@ -1,16 +1,25 @@
- public boolean hasCycle() {
+//判断是否有环 
+    public boolean hascycle(){
         Node fast = this.head;
         Node slow = this.head;
-        while(fast != null && fast.next != null) {
+        while (fast!=null&&fast.next!=null){
             fast = fast.next.next;
             slow = slow.next;
-            if(fast == slow) {
-                //return true;
-                break;
-            }
+            if (fast==slow){
+                return true;
         }
-        if(fast != null && fast.next != null) {
-            return true;
-        }
+
+    }
+
         return false;
+}
+
+
+//造环
+    public void createLoop() {
+        Node cur = this.head;
+        while(cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = this.head.next.next;
     }
