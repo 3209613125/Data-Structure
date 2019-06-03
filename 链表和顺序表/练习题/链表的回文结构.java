fbd0dc7461d8dc7a988b70c5dec80df4fd81e3ae
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-06-03 18:18:52 +0800

    Update 链表的回文结构.java

--- 链表和顺序表/练习题/链表的回文结构.java
+++ 链表和顺序表/练习题/链表的回文结构.java
@@ -1,13 +1,35 @@
- public boolean chkPalindrome() {
-        Node fast = this.head;
-        Node slow = this.head;
+对于一个链表，请设计一个时间复杂度为O(n),额外空间复杂度为O(1)的算法，判断其是否为回文结构。
+
+给定一个链表的头指针A，请返回一个bool值，代表其是否为回文结构。保证链表长度小于等于900。
+
+测试样例：
+1->2->2->1
+返回：true
+
+
+import java.util.*;
+
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class PalindromeList {
+    public boolean chkPalindrome(ListNode A) {
+        // write code here    public boolean chkPalindrome() {
+        ListNode fast =  A;
+        ListNode slow =  A;
         while(fast != null && fast.next != null){
             fast = fast.next.next;
             slow = slow.next;
         }
         //slow指向的位置就是中间节点
-        Node p = slow.next;
-        Node pNext = p.next;
+        ListNode p = slow.next;
+        ListNode pNext = p.next;
         while(p != null) {
             p.next = slow;
             slow = p;
@@ -17,17 +39,19 @@
             }
         }
         //后半部分已经进行反转
-        while(this.head != slow) {
-            if(this.head.data != slow.data){
+        while(A != slow) {
+            if(A.val != slow.val){
                 return false;
             }
 
-            if(this.head.next == slow) {
+            if(A.next == slow) {
                 return true;
             }
 
-            head = head.next;
+            A = A.next;
             slow = slow.next;
         }
         return true;
+
     }
+}
