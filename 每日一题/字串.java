0cab44158fa2133e5afb0cd547af68700ec1cb02
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-06-11 11:39:44 +0800

    Create 字串.java

---
+++ 每日一题/字串.java
@@ -0,0 +1,13 @@
+给定一个字符数组和它的长度，再给定一个字符串，判断该字符串是否为字符数组的字串
+
+
+class Substr {
+    public boolean[] chkSubStr(String[] p, int n, String s) {
+        // write code here
+        boolean[] res = new boolean[n];
+        for(int i = 0;i < n;i++){
+            res[i] = s.contains(p[i]);
+        }
+        return res;
+    }
+}
