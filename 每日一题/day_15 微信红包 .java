1b56b42e5f66887b9c52484a5fe527af6ac102cf
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-06-13 19:13:01 +0800

    Update day_15 微信红包 .java

--- 每日一题/day_15 微信红包 .java
+++ 每日一题/day_15 微信红包 .java
@@ -36,3 +36,28 @@ public class Gift {
         return (size>n/2)?temp:0;
     }
 }
+
+
+
+
+思路二：
+数据可能有不存在超过红包一半的，这个时候就要输出0；即该金额的数目必须 大于 红包数一半，不能小于
+等于。
+如果一个数出现次数超过一半了，排序过后，必然排在中间，则最后遍历整个排序后的数组查看是否符合条
+件。
+import java.util.*;
+public class Gift {
+public int getValue(int[] gifts, int n) {
+Arrays.sort(gifts);//数组排序，采用Arrays的sort方法
+int ans = gifts[n/2];//理论上超过半数的数字
+int num = 0;
+//遍历数组，进行统计
+for(int i = 0; i < gifts.length; i++) {
+if(gifts[i] == ans) {
+num++;
+}
+}
+return num <= n/2 ? 0 : ans;
+}
+}
+
