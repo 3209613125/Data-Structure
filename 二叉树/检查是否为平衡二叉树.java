95135156928011e0a8a2316ebd9b4f8bf385a666
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-06-13 20:19:28 +0800

    Update 检查是否为平衡二叉树.java

--- 二叉树/检查是否为平衡二叉树.java
+++ 二叉树/检查是否为平衡二叉树.java
@@ -1,4 +1,5 @@
      //判断是否为平衡二叉树，左右子树的高度之差不超过 "1"(大根本身是平衡二叉树，左右子树也必须是平衡二叉树)
+     //时间复杂度为n^2
      public boolean isBanlanced(TreeNode root){
          if(root==null){
              return  true;
@@ -23,3 +24,49 @@
          return leftHeight>rightHeight ? leftHeight+1:rightHeight+1;
      }
 
+
+//2.复杂度为O(n)的解法:
+
+class IsBalance{
+    public int height;
+    public boolean blance;
+}
+
+class Solution {
+    
+    public IsBalance function(TreeNode root){
+       
+        IsBalance ret = new IsBalance();
+        ret.height = 0;
+        ret.blance = true;
+        
+        if(root == null){
+            return ret;
+        }
+        
+        IsBalance lret = new IsBalance();
+       
+        lret = function(root.left);
+        if(lret.blance == false){//左子树不平衡
+            return lret;
+        }
+        
+        IsBalance rret = new IsBalance();
+        rret = function(root.right);
+        if(rret.blance == false){//左子树不平衡
+            return rret;
+        }
+        
+        
+        ret.blance = Math.abs(lret.height - rret.height) < 2;
+        
+        ret.height = lret.height > rret.height ? lret.height+1 : rret.height+1;
+        return ret;
+        
+    }
+    
+    public boolean isBalanced(TreeNode root) {
+        return function(root).blance;
+    }
+   
+}
