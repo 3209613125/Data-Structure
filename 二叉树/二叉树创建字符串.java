8cd402b08711c4a7f489b26613008d0ea6699318
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-06-15 14:56:04 +0800

    Update 二叉树创建字符串.java

--- 二叉树/二叉树创建字符串.java
+++ 二叉树/二叉树创建字符串.java
@@ -1,4 +1,8 @@
 //二叉树创建字符串
+//思路是:你需要采用前序遍历的方式，将一棵二叉树转换为一个由括号及整数组成的字符串
+//空结点则用一对空括号来表示"()"
+
+//非递归完成:
 class Solution {
     public String tree2str(TreeNode t) {
         StringBuilder sb = new StringBuilder();
@@ -32,3 +36,24 @@ class Solution {
     }
     
 }
+
+
+//递归完成：
+     public String CreateStr(TreeNode t){
+         if(t==null){
+             return "";
+
+         }
+         if(t.left==null&&t.right==null){
+             return t.value+"";
+         }
+         if(t.left==null){
+             return t.value+"()"+"("+CreateStr(t.right)+")";
+         }
+         if(t.right==null){
+             return t.value+"("+CreateStr(t.left)+")";
+         }
+
+         return t.value+"("+CreateStr(t.left)+")"+"("+CreateStr(t.right)+")";
+
+     }
