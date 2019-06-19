e19310bd21c09355d755d303aa5404e7d4031aa7
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-06-19 14:44:44 +0800

    Update 冒泡排序法.java

--- 排序/冒泡排序法.java
+++ 排序/冒泡排序法.java
@@ -27,20 +27,20 @@ class Solution{
 }
 
 
-优化:package www.bittech;
+package www.bittech;
 
 //冒泡排序法
-//优化:如果数组已经从小到大排序好，则内部for循环不用遍历第二遍
+//时间复杂度，o(n^2)
+//空间复杂度:稳定
 class Solution{
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,0};
+        int[] arr = {1, 2, 3, 4, 6, 5};
         bubble_sort(arr);
     }
-
     public static void bubble_sort(int arr[]){
         int arr_length = arr.length;
-        int count = 0;
         for(int i = 0;i < arr_length;arr_length--){
+           int count = 0;
             for(int j = i+1;j<arr_length;j++){
                 if(arr[j]<arr[j-1]){
                     int temp = arr[j-1];
