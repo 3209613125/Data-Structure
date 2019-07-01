f07a2a5864f7bf7a2490b19ea1b11cd78044599d
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-01 22:01:57 +0800

    Create 按要求将整数输出.java

---
+++ 每日一题/按要求将整数输出.java
@@ -0,0 +1,36 @@
+//第一个输出取各位和
+//第二个输出取各位平方和
+import  java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNextInt()){
+            int num = scan.nextInt();
+            sum(num);
+            pow_sum(num);
+        }
+
+        scan.close();
+
+
+    }
+    public static int sum( int num){
+        int count = 0;
+        while(num/10!=0){
+            count += num%10;
+            num = num/10;
+        }
+        count += num;
+        return count;
+    }
+
+    public static int pow_sum(int num){
+        int count1 = 0;
+        while(num/10!=0){
+            count1 += Math.pow(num%10,2);
+            num = num/10;
+        }
+        count1 += Math.pow(num,2);
+        return count1;
+    }
+}
