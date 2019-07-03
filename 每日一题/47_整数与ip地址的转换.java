3ac8f2f9125ac87f3a6ab63c0113a4728bb9ff39
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-03 16:44:33 +0800

    Update 47_整数与ip地址的转换.java

--- 每日一题/47_整数与ip地址的转换.java
+++ 每日一题/47_整数与ip地址的转换.java
@@ -1,3 +1,4 @@
+package www.hhy;
 import java.util.Scanner;
 
 
@@ -7,8 +8,9 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String str = "";
 
-        while (scanner.hasNext()) {
-            str = scanner.next();
+//        while (scanner.hasNext()) {
+//            str = scanner.next();
+        str = "215561";
             if (str.contains(".")) {
                 System.out.println(ipToInt(str));
             } else {
@@ -16,13 +18,16 @@ public class Main {
             }
         }
 
-        scanner.close();
-    }
+//        scanner.close();
+//    }
 
     private static String intToIp(String str) {
         String result = "";
         Long input = Long.parseLong(str);
         for (int i = 3; i >= 0; i--) {
+
+            //就相当于全部转化为二进制之后再进行"与"运算
+            //& 0x000000ff直接作用就是只保留最后8位(一个16进制位是4个2进制位),屏蔽掉前面的数字
             result = (input & 0x000000FF) + "." + result;
             input >>>= 8;
         }
@@ -35,8 +40,19 @@ public class Main {
         array = str.split("[.]");
         for (String s : array) {
             result = (result << 8) + Integer.parseInt(s);
+
         }
         return result;
     }
 
 }
+
+
+
+//        int result = 8;
+//        result = (result<<2);
+
+
+//        int d = Integer.parseInt("00001010000000000000001111000001",2);
+
+//        System.out.println((int) Math.pow(2,27));
