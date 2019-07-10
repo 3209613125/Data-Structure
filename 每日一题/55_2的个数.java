701823b6a96747ced198b7efbdecd84b4945c005
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-10 18:50:22 +0800

    Update 55_2的个数.java

--- 每日一题/55_2的个数.java
+++ 每日一题/55_2的个数.java
@@ -1,41 +1,30 @@
-
 import java.util.Scanner;
- 
-public class Main {
-    public static void findMoney(String p, String a)
-    {
-        //两个常亮用来表示各个单位到最小单位之间的进制关系
-        final int FIRST = 17*29;
-        final int SECOND = 29;
-        
-        String[] ps = p.split("\\.");
-        int l1 = ps.length;
-        int moneyP = (l1>=1?Integer.parseInt(ps[0])*FIRST:0) +
-                (l1>=2?Integer.parseInt(ps[1])*SECOND:0) +
-                (l1>=3?Integer.parseInt(ps[2]):0); //5421
- 
-        String[] as = a.split("\\.");
-        int l2 = as.length;
-        int moneyA =  (l2>=1?Integer.parseInt(as[0])*FIRST:0) +
-                (l2>=2?Integer.parseInt(as[1])*SECOND:0) +
-                (l2>=3?Integer.parseInt(as[2]):0); //
-        //找的钱数
-        int find = moneyA - moneyP;
-        if (find < 0)//如果是负数，在最前面先输出一个负号，然后把它当正数处理
-        {
-            find *= (-1);
-            System.out.print("-");
+
+ public class Count2 {
+    public int countNumberOf2s(int n) {
+        int count = 0;
+        for( int i = 0;i <= n;i++){
+           if( String.valueOf(i).contains("2")){
+               for (int i1 = 0;i1<String.valueOf(i).length();i1++){
+                   if(String.valueOf(i).charAt(i1)=='2'){
+                       count ++;
+
+                   }
+               }
+            }
+
         }
-        System.out.println(find/(FIRST)+"."+(find%FIRST)/SECOND+"."+find%FIRST%SECOND);
+        return  count;
     }
-    public static void main(String[] args) {
- 
-        Scanner s = new Scanner(System.in);
-        while (s.hasNext())
-        {
-            String p = s.next();
-            String a = s.next();
-            findMoney(p, a);
-        }
-    }
+
+
 }
+class Test{
+    public static void main(String[] args) {
+        Count2 count2 = new Count2();
+        Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+            System.out.println(count2.countNumberOf2s(n));
+        
+    }
+ }
