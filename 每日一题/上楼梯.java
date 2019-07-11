d2a204e3a1cfe7544d61fb2118f78ac3c24447e3
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-11 23:51:32 +0800

    Create 上楼梯.java

---
+++ 每日一题/上楼梯.java
@@ -0,0 +1,48 @@
+//1
+
+public class Main {
+    public static int countWays(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 4;
+        int mod = 1000000007;
+        int first = 1;
+        int second = 2;
+        int third = 4;
+        int result = 0;
+        for (int i = 4; i <= n; i++) {
+            result = (first + second) % mod + third % mod;
+            first = second % mod;//依次往后移动一位数
+            second = third % mod;
+            third = result;
+        }
+        return result % mod;
+    }
+}
+
+
+
+//2(迭代)
+/*
+思路：上1级有一种，2级上1级后再上1级，3级有上1级之后上2级或上2级之后上1级或直接上3级
+4级有1级上3级，2级上2级，3级上1级
+总结起来有：f(1)=1,f(2)=2,f(3)=4,f(4)=f(1)+f(2)+f(3);f(5)=f(2)+f(3)+f(4)
+f(n)=f(n-1)+f(n-2)+f(n-3);
+本来使用递归做的，结果递归做到最后超时了，只好用迭代了；
+*/
+ class GoUpstairs {
+    public int countWays(int n) {
+        if(n<1) return 0;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(n==3) return 4;
+        int array[] =new int[n];
+        array[0] =1;
+        array[1] =2;
+        array[2] =4;
+        for(int i=3;i<n;i++){
+            array[i]=((array[i-1]+array[i-2])%1000000007+array[i-3]%1000000007)%1000000007;
+        }
+        return array[n-1];
+    }
+}
