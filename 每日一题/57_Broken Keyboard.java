fd6ced6ee6b2fab2106b530a19783f4e202967be
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-11 19:57:37 +0800

    Create 57_Broken Keyboard.java

---
+++ 每日一题/57_Broken Keyboard.java
@@ -0,0 +1,28 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+        String s1 = scanner.next().toUpperCase();
+        String s2 = scanner.next().toUpperCase();
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        Set<Character> set1 = new LinkedHashSet<>();
+        Set<Character> set2 = new LinkedHashSet<>();
+        for (int i = 0; i < c1.length; i++) {
+            set1.add(c1[i]);
+        }
+        for (int i = 0; i < c2.length; i++) {
+            set2.add(c2[i]);
+        }
+        for (Character c : set2)
+            set1.remove(c);
+        for (Character c : set1)
+            System.out.print(c);
+    }
+}
+}
