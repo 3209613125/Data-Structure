2664cc23b3ed82910fc6310d53bef56823b01114
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-15 23:31:02 +0800

    Update 用两个栈实现队列.java

--- 栈和队列/用两个栈实现队列.java
+++ 栈和队列/用两个栈实现队列.java
@@ -19,3 +19,51 @@ public class TestMyQueue {
         return stack2.pop();
     }
 }
+
+
+-------------------------------------------------------------------------------------------------------
+    
+    
+    import java.util.*;
+
+/**
+ * @Information:
+ * @Author: HeHaoYuan
+ * @Date: Created at 16:46 on 2019/7/14
+ * @Package_Name: www.hhy.java_7
+ */
+public class lianxi {
+    public static void main(String[] args) {
+
+        Test test = new Test();
+        int[]a = {1,2,3,4,5};
+        test.push(a);
+        test.pop1();
+        int size = test.stack1.size();
+        for (int i = 0;i < size ;i++){
+            System.out.print(test.stack1.pop()+" ");
+        }
+
+
+
+
+    }
+}
+class Test{
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> stack1 = new Stack<>();
+    public void push(int[] a){
+        for(int i = 0;i < a.length;i++){
+            stack.push(a[i]);
+        }
+
+    }
+
+    public void pop1(){
+        if(stack1.isEmpty()){
+            while (!stack.empty()){
+                stack1.push(stack.pop());
+            }
+        }
+    }
+}
