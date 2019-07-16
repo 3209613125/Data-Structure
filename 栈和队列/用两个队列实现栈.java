9d8e1f2672ad0141a8ae5e53a4af2dc5adaf7ee9
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-16 16:12:30 +0800

    Update 用两个队列实现栈.java

--- 栈和队列/用两个队列实现栈.java
+++ 栈和队列/用两个队列实现栈.java
@@ -71,3 +71,67 @@ public class TestMyStack {
         return this.usedSize == 0;
     }
 }
+
+
+
+
+---------------------------------------------------------------------------------------------------------
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyQueue {
+    private Queue<Integer> queue;
+    private Queue<Integer> queue1;
+    private int usedSize;
+
+
+    public MyQueue() {
+        this.queue = new LinkedList<>();
+        this.queue1 = new LinkedList<>();
+        this.usedSize = 0;
+    }
+
+    public void push(int[] data) {
+        if (queue.isEmpty()) {
+            for (int i = 0; i < data.length; i++) {
+                queue.add(data[i]);
+            }
+        }
+    }
+
+    public void poll() {
+        while (true) {
+
+            while(queue.size() > 1) {
+                queue1.add(queue.poll());
+            }
+            if (queue.size() == 1) {
+                System.out.println(queue.peek());
+                queue.remove();
+                //System.out.println(queue.poll());;Queue的peek方法不会弹出队头元素，只会显示，不会弹出
+            }
+
+            while (queue1.size() > 1) {
+                queue.add(queue1.poll());
+            }
+            if (queue1.size() == 1) {
+                System.out.println(queue1.peek());
+                queue1.remove();
+                //System.out.println(queue1.poll());
+            }
+            if (queue.size() == 0 && queue1.size() == 0) {
+                break;
+            }
+        }
+
+    }
+}
+class TestDemo{
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        int[] data = {1,2,3,4,5};
+        myQueue.push(data);
+        myQueue.poll();
+
+    }
+}
