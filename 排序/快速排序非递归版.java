5ed8ac9315ae481d122bc0ab282c9c2f4e7d3e73
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-18 00:54:46 +0800

    Update 快速排序非递归版.java

--- 排序/快速排序非递归版.java
+++ 排序/快速排序非递归版.java
@@ -1,1 +1,28 @@
-
+public void quickSort_1(int[] array){
+    Stack<Integer> stack = new Stack<>();
+    int low = 0;
+    int high = array.length-1;
+    int par = FindPartion(array,low,high);
+    if(par > low+1){
+        stack.push(low);
+        stack.push(par-1);
+    }
+    if(par < high-1){
+        stack.push(par+1);
+        stack.push(high);
+    }
+    while(!stack.empty()){
+        high = stack.pop();
+        low = stack.pop();
+        par = FindPartion(array,low,high);
+        if(par > low+1){
+            stack.push(low);
+            stack.push(par-1);
+        }
+        if(par < high-1){
+            stack.push(par+1);
+            stack.push(high);
+        }
+    }
+  }
+}
