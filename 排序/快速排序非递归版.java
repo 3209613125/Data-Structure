a96e41f8180d8f73d2b59dd583f873a1a2c94d57
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-25 16:43:19 +0800

    Update 快速排序非递归版.java

--- 排序/快速排序非递归版.java
+++ 排序/快速排序非递归版.java
@@ -1,28 +1,47 @@
-public void quickSort_1(int[] array){
-    Stack<Integer> stack = new Stack<>();
-    int low = 0;
-    int high = array.length-1;
-    int par = FindPartion(array,low,high);
-    if(par > low+1){
-        stack.push(low);
-        stack.push(par-1);
-    }
-    if(par < high-1){
-        stack.push(par+1);
-        stack.push(high);
-    }
-    while(!stack.empty()){
-        high = stack.pop();
-        low = stack.pop();
-        par = FindPartion(array,low,high);
-        if(par > low+1){
-            stack.push(low);
-            stack.push(par-1);
-        }
-        if(par < high-1){
-            stack.push(par+1);
-            stack.push(high);
-        }
-    }
-  }
+public static int partion(int[] array, int low, int high) {
+	int temp = array[low];
+	while (low < high) {
+		while (low < high && array[high] > temp) {
+			high--;//high有可能会越界
+		}
+		array[low] = array[high];
+		while (low < high && array[low] < temp) {
+			low++;
+		}
+		array[high] = array[low];
+	}
+	array[low] = temp;
+	return low;
+}
+
+public static void quickSort2(int[] array) {
+	int low = 0;
+	int high = array.length-1;
+	Stack<Integer> stack = new Stack<>();
+
+	int par = partion(array, low, high);
+
+	if (par > low + 1) {
+		stack.push(low);
+		stack.push(par - 1);
+	}
+	if (par < high - 1) {
+		stack.push(par + 1);
+		stack.push(high);
+	}
+
+	while (!stack.isEmpty()) {
+		high = stack.pop();
+		low = stack.pop();
+		par = partion(array, low, high);
+
+		if (par > low+1) {
+			stack.push(low);
+			stack.push(par-1);
+		}
+		if (par < high-1) {
+			stack.push(par+1);
+			stack.push(high);
+		}
+	}
 }
