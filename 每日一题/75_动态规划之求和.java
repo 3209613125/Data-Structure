158f449f7dcfeae88cc67ca4674a827d48b8fa1b
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-30 11:06:37 +0800

    Update 75_动态规划之求和.java

--- 每日一题/75_动态规划之求和.java
+++ 每日一题/75_动态规划之求和.java
@@ -1,3 +1,24 @@
+链接：https://www.nowcoder.com/questionTerminal/11cc498832db489786f8a03c3b67d02c
+来源：牛客网
+
+输入两个整数n和m，从数列1,2,3 ....... n中随意取几个数，使其和等于m，要求将其中所有的可能组合列出来
+
+输入描述：
+每个测试输入包含2个整数，N和米
+
+
+输出描述：
+按每个组合的字典序排列输出，每行输出一种组合
+示例1
+输入
+5 5
+输出
+1 4 
+2 3 
+5
+
+
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
