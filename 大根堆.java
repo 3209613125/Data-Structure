ee5354ba3acc115dcd59012cd476dbd8f210ce48
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-31 10:39:39 +0800

    Update 大根堆.java

--- 大根堆.java
+++ 大根堆.java
@@ -1,21 +1,4 @@
-public interface IHeap {
-    //向下调整
-    void AdjustDown(int root,int len);
-    //初始化建立大根堆
-    public void initHeap(int[] array);
-    //向上调整，从孩子节点开始调整
-    void AdjustUp(int child,int len);
-    // 插入 item 到堆中
-    void pushHeap(int item);
-    // 返回堆顶元素，删除数据元素
-    int popHeap();
-    // 返回堆顶元素，不删除数据元素
-    int getHeapTop();
-    //堆排序
-    void HeapSort();
-    //打印堆
-    void show();
-}
+import java.util.Arrays;
 
 
 public class TestHeap implements IHeap{
@@ -63,65 +46,80 @@ public class TestHeap implements IHeap{
         }
     }
 
- @Override
-    public void AdjustUp(int child, int len) {
+    @Override
+    public void AdjustUp(int child) {
         int parent = (child-1)/2;
-        while (child>0){
-            if(this.elem[child]>this.elem[parent]) {
-                int tmp = this.elem[child];
-                this.elem[child] = this.elem[parent];
-                this.elem[parent] = tmp;
+        while (child > 0){
+            if(elem[child] > elem[parent]){
+                int tmp = elem[child];
+                elem[child] = elem[parent];
+                elem[parent] = tmp;
                 child = parent;
                 parent = (child-1)/2;
-
-            }else{
-                    break;
-                }
+            }else {
+                break;
             }
         }
-
-     @Override
+    }
+    public boolean isFull() {
+        return this.usedSize == this.elem.length;
+    }
+    @Override
     public void pushHeap(int item) {
         if(isFull()){
-            this.elem = Arrays.copyOf(this.elem,2*this.elem.length);
+            this.elem = Arrays.copyOf(this.elem,
+                    2*this.elem.length);
         }
+        this.elem[this.usedSize] = item;
+        this.usedSize++;
+        AdjustUp(this.usedSize-1);
     }
-
+    public boolean isEmpty() {
+        return this.usedSize == 0;
+    }
     @Override
     public int popHeap() {
-        return 0;
+        if(isEmpty()){
+            throw new UnsupportedOperationException("堆为空");
+        }
+        int oldData = elem[0];
+        int tmp = elem[0];
+        elem[0] = elem[this.usedSize-1];
+        elem[this.usedSize-1] = tmp;
+        this.usedSize--;
+        AdjustDown(0,this.usedSize);
+        return oldData;
     }
 
     @Override
     public int getHeapTop() {
-        return 0;
-    }
-    
-    public boolean isEmpty(){
-        return  this.usedSize==0;
-}
-    
-     private boolean isFull(){
-        return  this.usedSize == this.elem.length;
+        if(isEmpty()){
+            throw new UnsupportedOperationException("堆为空");
+        }
+        return elem[0];
     }
 
-
+    /**
+     * 时间复杂度：
+     * O(n log 2n)
+     * 空间复杂度:o(1)
+     * 稳定性：不稳定
+     */
     @Override
-    //时间复杂度：n*log 2 n
-    //空间复杂度：O（1）
-    //稳定性：不稳定
     public void HeapSort() {
+       /* for (int i = (array.length-1-1)/2; i >= 0 ; i--) {
+            AdjustDown(i,this.usedSize);
+        }*/
         int end = this.usedSize-1;
-        while (end>0){
+        while (end > 0) {
             int tmp = elem[0];
-            elem[0]=elem[end];
-            elem[end]=tmp;
+            elem[0] = elem[end];
+            elem[end] = tmp;
             AdjustDown(0,end);
             end--;
-
         }
     }
-
+    //  topK     作业：一亿个数据找出十个最大/最小的数据
     @Override
     public void show() {
         for (int i = 0; i < this.usedSize; i++) {
@@ -132,11 +130,21 @@ public class TestHeap implements IHeap{
 }
 
 
-public class TestDemo {
-    public static void main(String[] args) {
-        TestHeap testHeap = new TestHeap();
-        int[] array = {1,2,3,4,5,6,7,8,9,10};
-        testHeap.initHeap(array);
-        testHeap.show();
-    }
+ interface IHeap {
+    //向下调整
+    void AdjustDown(int root,int len);
+    //初始化建立大根堆
+    public void initHeap(int[] array);
+    //向上调整，从孩子节点开始调整
+    void AdjustUp(int child);
+    // 插入 item 到堆中
+    void pushHeap(int item);
+    // 返回堆顶元素，删除数据元素
+    int popHeap();
+    // 返回堆顶元素，不删除数据元素
+    int getHeapTop();
+    //堆排序
+    void HeapSort();
+    //打印堆
+    void show();
 }
