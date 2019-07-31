0fa07011a2bdc0007ff369871391810b2e269fc8
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-07-31 09:42:46 +0800

    Update 快速排序非递归版.java

--- 排序/快速排序非递归版.java
+++ 排序/快速排序非递归版.java
@@ -1,47 +1,54 @@
-public static int partion(int[] array, int low, int high) {
-	int temp = array[low];
-	while (low < high) {
-		while (low < high && array[high] > temp) {
-			high--;//high有可能会越界
-		}
-		array[low] = array[high];
-		while (low < high && array[low] < temp) {
-			low++;
-		}
-		array[high] = array[low];
-	}
-	array[low] = temp;
-	return low;
-}
+class QuictSort{
+    public static void main(String[] args) {
+        int[] array = new int[]{6,5,4,9,8};
+        sort(array);
+        for (int i : array){
+            System.out.print(i+" ");
+        }
+    }
 
-public static void quickSort2(int[] array) {
-	int low = 0;
-	int high = array.length-1;
-	Stack<Integer> stack = new Stack<>();
+    public static int partation(int[] arrray,int low,int high) {
+        int temp = arrray[low];
+        while (low < high) {
+            while (low < high && arrray[high] > temp) {
+                high--;
+            }
+            arrray[low] = arrray[high];
 
-	int par = partion(array, low, high);
+            while (low < high && arrray[low] < temp) {
+                low++;
+            }
+            arrray[high] = arrray[low];
+        }
+        arrray[high] = temp;
+        return low;
+    }
 
-	if (par > low + 1) {
-		stack.push(low);
-		stack.push(par - 1);
-	}
-	if (par < high - 1) {
-		stack.push(par + 1);
-		stack.push(high);
-	}
-
-	while (!stack.isEmpty()) {
-		high = stack.pop();
-		low = stack.pop();
-		par = partion(array, low, high);
-
-		if (par > low+1) {
-			stack.push(low);
-			stack.push(par-1);
-		}
-		if (par < high-1) {
-			stack.push(par+1);
-			stack.push(high);
-		}
-	}
+    public static void sort(int[] array) {
+        int low = 0;
+        int high = array.length - 1;
+        Stack<Integer> stack = new Stack<>();
+        int par = partation(array, low, high);
+        if (par>low+1){
+            stack.push(low);
+            stack.push(par-1);
+        }
+        if (par<high-1){
+            stack.push(par+1);
+            stack.push(high);
+        }
+        while (!stack.isEmpty()){
+            high = stack.pop();
+            low = stack.pop();
+            par = partation(array,low,high);
+            if (par>low+1){
+                stack.push(low);
+                stack.push(par-1);
+            }
+            if (par<high-1){
+                stack.push(par+1);
+                stack.push(high);
+            }
+        }
+    }
 }
