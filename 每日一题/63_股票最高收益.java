1218b4c84798f6e32ff8242ddf30b4de56045098
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-08-10 16:41:06 +0800

    Update 63_股票最高收益.java

--- 每日一题/63_股票最高收益.java
+++ 每日一题/63_股票最高收益.java
@@ -1,24 +1,26 @@
-class Solution {
-    public static int calculateMax(int[] prices) {
-        int firstBuy = Integer.MAX_VALUE; // 第一次买入最好的价格，越低越好
-        int firstSell = 0; // 第一次卖出后的最高收益，越高越好
-        int secondBuy = Integer.MIN_VALUE; // 第二次买入时还剩余的最高收益，越高越好
-        int secondSell = 0; // 第二次卖出时总的最高收益，越高越好
-        for (int price : prices) {
-// 当前价格下第一次买入的价格
-            firstBuy = Math.min(firstBuy, price);
-// 当前价格 - 买入价格 就是当前价格下第一次买卖的收益
-            firstSell = Math.max(firstSell, price - firstBuy);
-// 第一次卖出的收益 - 当前价格，即当前价格下，第二次买入后还剩余收益
-            secondBuy = Math.max(secondBuy, firstSell - price);
-// 剩余收益 + 当前价格，即当前价格下，第二次买卖的收益
-            secondSell = Math.max(secondSell, secondBuy + price);
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] prices = new int[n];
+        for (int i = 0; i < n; i++) {
+            prices[i] = scanner.nextInt();
         }
-        return secondSell;
+        System.out.println(calculateMax(prices));
     }
 
-    public static void main(String[] args) {
-        int[] prices = {3,8,5,1,7,8};
-        System.out.println(calculateMax(prices));
+    public static int calculateMax(int[] prices) {
+        int firstbuy = Integer.MAX_VALUE;
+        int firstsell = 0;
+        int secondbuy = Integer.MIN_VALUE;
+        int secondsell = 0;
+        for (int price : prices) {
+            firstbuy = Math.min(firstbuy, price);
+            firstsell = Math.max(firstsell, price - firstbuy);
+            secondbuy = Math.max(secondbuy, firstsell - price);
+            secondsell = Math.max(secondsell, price + secondbuy);
+        }
+        return secondsell;
     }
 }
