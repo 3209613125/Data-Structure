b7385097d1589885da3f753529dfd4770e6f4642
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-08-11 18:56:15 +0800

    Update 26_ 子串.java

--- 每日一题/26_ 子串.java
+++ 每日一题/26_ 子串.java
@@ -1,21 +1,6 @@
 给定一个字符数组和它的长度，再给定一个字符串，判断该字符串是否为字符数组的子串
 
-
-class Substr {
-    public boolean[] chkSubStr(String[] p, int n, String s) {
-        // write code here
-        boolean[] res = new boolean[n];
-        for(int i = 0;i < n;i++){
-            res[i] = s.contains(p[i]);
-        }
-        return res;
-    }
-}
-
-
-
-
-测试： class Substr {
+ class Substr {
      public static void main(String[] args) {
          String[] p = new String[]{"res","tre","yes"};
          int n = 3;
