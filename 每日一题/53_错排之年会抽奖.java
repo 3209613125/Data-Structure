03b9cba4c0a022f5cec40ad812094ecabcaa123c
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-08-24 23:06:33 +0800

    Update 53_错排之年会抽奖.java

--- 每日一题/53_错排之年会抽奖.java
+++ 每日一题/53_错排之年会抽奖.java
@@ -1,3 +1,8 @@
+//错排
+//设第n步的放法有D（n）种
+//第一步：n个元素放到k位置，有n-1种放法
+//第二步：n-1个元素中k若放到n位置，剩下的n-2个元素，有n-2种放法；若不放到n为值，剩下的n-1个为值有n-1种放法
+//综上所述：D（n） = （n-1）（D（n-1）+D（n-2））
 import java.util.*;
 public class Main {
 public static void main(String[] args) {
