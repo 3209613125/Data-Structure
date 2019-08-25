ea8db16dc80c0cb06635df00c800fd695a7533f1
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-08-25 10:08:33 +0800

    Update 53_错排之年会抽奖.java

--- 每日一题/53_错排之年会抽奖.java
+++ 每日一题/53_错排之年会抽奖.java
@@ -3,38 +3,42 @@
 //第一步：n个元素放到k位置，有n-1种放法
 //第二步：n-1个元素中k若放到n位置，剩下的n-2个元素，有n-2种放法；若不放到n为值，剩下的n-1个为值有n-1种放法
 //综上所述：D（n） = （n-1）（D（n-1）+D（n-2））
-import java.util.*;
-public class Main {
-public static void main(String[] args) {
-// 输入
-Scanner scan = new Scanner(System.in);
-while(scan.hasNext()){
-int n = scan.nextInt(); // 代表测试数据的组数
-float sum1 = factorial(n);
-float sum2 = count(n);
-//将得到的分子分母进行相除，就可以得到概率了。
-float result1 = (sum2/sum1)*100;
-System.out.println(String.format("%.2f", result1) + "%");
-}
-}
+import java.util.Scanner;
 
-public static float count(int n) {
-if(n==1){
-return 0;
-}else if(n==2){
-return 1;
-}else{
-return (n-1)*(count(n-1)+count(n-2));
-}
-}
+//新年抽奖
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+     
+        while (scanner.hasNext()){
+            int n = scanner.nextInt();
+            float a = fenmu(n);
+            float b = fenzi(n);
+            float result = (b/a)*100;
+            System.out.println(String.format("%.2f",result)+"%");
+        }
+       
 
-public static float factorial(int num) {
-float result = 1;
-if(num==0){
-return 1;
-}else if (num > 0) {
-result = num * factorial(num - 1);
-}
-return result;
-}
+
+    }
+    public static float fenzi(int n){
+        if (n == 1){
+            return 0;
+        }
+        if (n == 2){
+            return 1;
+        }
+        else {
+            return (n-1)*(fenzi(n-1)+fenzi(n-2));
+        }
+
+    }
+    public static float fenmu(int n){
+        if (n == 0){
+            return 1;
+        }
+           return  n*fenmu(n-1);
+
+    }
+
 }
