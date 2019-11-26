27ac42ec3277f9973c8f0ad5d04a6863ff248fa6
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2019-11-26 09:56:25 +0800

    Update 斗地主的洗牌和发牌.java

--- 斗地主的洗牌和发牌.java
+++ 斗地主的洗牌和发牌.java
@@ -59,3 +59,81 @@ public class poker {
     }
 
 }
+
+
+
+
+
+----------------------------------------------------------------------------------------------------------------------------
+    
+    
+    
+    
+    
+    
+    
+    public class poker {
+    public static void main(String[] args) {
+        //拼接花色和数字
+        String[] color = {"方片","红桃","梅花","黑桃"};
+        String[] num = {"3","4","5","6","7","8","9","10","J","Q","K","A","2"};
+
+        HashMap<Integer,String> poker = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        int index = 0;
+        for (String s1 : num){
+            for (String s2 : color){
+                poker.put(index,s2.concat(s1));
+                list.add(index);
+                index++;
+            }
+        }
+        poker.put(index,"小王");
+        list.add(index);
+        index++;
+        poker.put(index,"大王");
+        list.add(index);
+
+        Collections.shuffle(list);
+
+
+        TreeSet<Integer> player1 = new TreeSet<>();
+        TreeSet<Integer> player2 = new TreeSet<>();
+        TreeSet<Integer> player3 = new TreeSet<>();
+        TreeSet<Integer> dipai = new TreeSet<>();
+
+        //发牌
+        for (int i = 0;i < poker.size();i++){
+            if (i >= poker.size()-3){
+                dipai.add(list.get(i));
+            }
+           else if (i % 3 == 0){
+                player1.add(list.get(i));
+            }
+            else if (i % 3 == 1){
+                player2.add(list.get(i));
+            }
+           else {
+                player3.add(list.get(i));
+            }
+        }
+
+        //显示牌
+        lookPai(poker,player1,"玩家一");
+        lookPai(poker,player2,"玩家二");
+        lookPai(poker,player3,"玩家三");
+        lookPai(poker,dipai,"底牌");
+
+
+    }
+    public static void lookPai(HashMap<Integer,String> poker,TreeSet<Integer> player,String name){
+        System.out.println(name+"是:");
+        for (int i : player){
+            System.out.print(poker.get(i)+" ");
+        }
+        System.out.println();
+
+    }
+
+
+}
