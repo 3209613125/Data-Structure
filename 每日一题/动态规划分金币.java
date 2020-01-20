68c365cafc8a0b9309ff563d4a6f391e21f95ba0
何浩源 <47206212+hehaoyuan@users.noreply.github.com>
2020-01-20 16:18:21 +0800

    Update 动态规划分金币.java

--- 每日一题/动态规划分金币.java
+++ 每日一题/动态规划分金币.java
@@ -1,3 +1,7 @@
+//给定一个n，问能组成w的钱币最少有多少种？f（n）= min（f（n-11），f（n-5），f（n-1））+1   n = 100
+//f(15) = f(15-11)+1 = f(4)+1 = f(3)+1+1 = 5
+//f(15) = f(15-5)+1 = f(10)+1 = f(10-5)+1+1 = f(5) +1 +1 = 3
+
 class Dynamic_Programming{
     public static void main(String[] args) {
        int n = 100;
